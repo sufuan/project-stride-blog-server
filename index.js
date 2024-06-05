a3d707daa9d03ac1489376a9c6d2b253b15704cc
sufuan <abu49539@gmail.com>
2024-06-05 23:51:09 +0600

    hhh

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ app.use(cors());
 app.use(express.json());
 
 // connect db
-
 ConnectDB();
+
+// Define route handler for the root domain
+app.get("/", (req, res) => {
+  res.send("Welcome to the root domain!");
+});
 
 app.get("/api/get", (req, res) => {
   res.send("Server is working!");
 });
 
+// Dynamically load all routes from the routes folder
 readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
 const port = process.env.PORT || 8000;
